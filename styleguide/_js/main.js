35ed4c97f70ad028a0c960ed04e601206c940ad1
Fred Marecesche <fred@froddd.co.uk>
2016-02-18 12:15:12 +0000

    Update JS include paths

--- styleguide/_js/main.js
+++ styleguide/_js/main.js
@@ -2,8 +2,8 @@
  * For the Style Guide, import everything
  */
 
-//= require ../../../bower_components/modernizr/modernizr.js
-//= require ../../../bower_components/foundation/js/foundation.min.js
-//= require ../../../bower_components/parsleyjs/dist/parsley.min.js
-//= require ../../../scripts/parsley-init.js
-//= require ../../../scripts/main.js
+//= require ../../bower_components/modernizr/modernizr.js
+//= require ../../bower_components/foundation/js/foundation.min.js
+//= require ../../bower_components/parsleyjs/dist/parsley.min.js
+//= require ../../scripts/parsley-init.js
+//= require ../../scripts/main.js
