14c07819efc28b2865e3622751a4b11df6c116b6
Fred Marecesche <fred@froddd.co.uk>
2016-03-10 09:14:04 +0000

    Bring in proper Toolkit JS

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,33 @@
 /**
- * For the Style Guide, import everything
+ * Co-op Front-end Toolkit Scripts
  */
 
-//= require ../../bower_components/modernizr/modernizr.js
-//= require ../../bower_components/foundation/js/foundation.min.js
-//= require ../../bower_components/parsleyjs/dist/parsley.min.js
-//= require ../../scripts/parsley-init.js
-//= require ../../scripts/main.js
+$(function() {
+  // Mobile menu
+  $('.mobile-menu-toggle').on('click', function() {
+    $('#navigation, .mobile-menu-toggle').toggleClass('open');
+    if ($('.mobile-menu-toggle').hasClass('open')) {
+      $('.mobile-menu-toggle .mobile-menu-text').text('Close');
+    }
+    else {
+      $('.mobile-menu-toggle .mobile-menu-text').text('Menu');
+    }
+  });
+
+  // Tabs
+  $('.tabs').on('click', '.tabs-nav a', function(e) {
+    e.preventDefault();
+    $(this).addClass('active').parent('li').siblings().find('a').removeClass('active');
+    $($(this).attr('href')).addClass('active').siblings().removeClass('active');
+  }).addClass('init').find('.tabs-nav li:first a').trigger('click');
+
+  // Toggles
+  $('.toggle-trigger').on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('open');
+    if ($(this).parent().hasClass('accordion')) {
+      $(this).siblings('.toggle-trigger').removeClass('open');
+    }
+  }).addClass('init');
+});
+
--- src/_js/main.js
+++ src/_js/main.js
@@ -1,9 +1,1 @@
-/**
- * For the Style Guide, import everything
- */
-
-//= require ../../bower_components/modernizr/modernizr.js
-//= require ../../bower_components/foundation/js/foundation.min.js
-//= require ../../bower_components/parsleyjs/dist/parsley.min.js
-//= require ../../scripts/parsley-init.js
-//= require ../../scripts/main.js
+//= require ../../bower_components/coop-frontend-toolkit/scripts/coop-toolkit.js
