e11aa3750c39a3d94568bd4d9347bd0279d56028
Fred Marecesche <fred@froddd.co.uk>
2016-03-17 17:03:22 +0000

    Update to new Coop Frontend Toolkit modules

--- src/_js/main.js
+++ src/_js/main.js
@@ -1,1 +1,8 @@
 //= require ../../bower_components/coop-frontend-toolkit/scripts/coop-toolkit.js
+//= require ../../bower_components/coop-frontend-toolkit/scripts/modules/coop.MenuToggle.js
+//= require ../../bower_components/coop-frontend-toolkit/scripts/modules/coop.Tabs.js
+//= require ../../bower_components/coop-frontend-toolkit/scripts/modules/coop.Toggles.js
+
+$(function() {
+  Coop.init();
+});
