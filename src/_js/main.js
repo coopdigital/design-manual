54110d0c80659cd8f7a97614d5469920a26712b1
Fred Marecesche <froddd@users.noreply.github.com>
2016-04-27 16:46:11 +0100

    Update to latest Toolkit Toggles JS module (#8)

--- src/_js/main.js
+++ src/_js/main.js
@@ -1,21 +1,7 @@
 //= require ../../node_modules/coop-frontend-toolkit/scripts/coop-toolkit.js
-//= require ../../node_modules/coop-frontend-toolkit/scripts/modules/coop.MenuToggle.js
 //= require ../../node_modules/coop-frontend-toolkit/scripts/modules/coop.Tabs.js
 //= require ../../node_modules/coop-frontend-toolkit/scripts/modules/coop.Toggles.js
 
 $(function() {
   Coop.init();
-
-  // Mock mobile menu functionality for the header example
-  $('.example .mobile-menu-toggle')
-    .off('click.MenuToggle')
-    .on('click', function(e) {
-      e.preventDefault();
-      if($(this).hasClass('open')) {
-        $(this).add('#navigation-example').removeClass('open').find('.mobile-menu-text').text('Menu');
-      }
-      else {
-        $(this).add('#navigation-example').addClass('open').find('.mobile-menu-text').text('Close');
-      }
-    });
 });
