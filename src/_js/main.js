03686d0bb08c5e3938c19a069dbfd1c0ef15384d
Matt Tyas <matthewtyas@hotmail.com>
2016-08-19 15:33:46 +0100

    form styles from wills ported over;

--- src/_js/main.js
+++ src/_js/main.js
@@ -1,7 +1,8 @@
 //= require ../../node_modules/coop-frontend-toolkit/scripts/coop-toolkit.js
 //= require ../../node_modules/coop-frontend-toolkit/scripts/modules/coop.Tabs.js
 //= require ../../node_modules/coop-frontend-toolkit/scripts/modules/coop.Toggles.js
+//= require ../../node_modules/coop-frontend-toolkit/scripts/modules/coop.TapCheck.js   
 
 $(function() {
   Coop.init();
-});
+});    
