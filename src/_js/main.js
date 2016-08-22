539d147cdb6fc9affa0fe62a61d528a907f5f977
Matt Tyas <matthewtyas@hotmail.com>
2016-08-22 22:31:27 +0100

    restyled manual

--- src/_js/main.js
+++ src/_js/main.js
@@ -6,3 +6,51 @@
 $(function() {
   Coop.init();
 });    
+
+// Responsive sidebar navigation
+
+var sidebar = $('.side-nav'),
+    sidebarTrigger = $('.nav-trigger'),
+    appWrap = $('.app-content'),
+    winHeight = $(document).height();
+
+// Different navigation pattern at different break points
+function openNavigation(){
+
+  sidebarTrigger.on('click', function(event){
+    // $(sidebar).toggleClass('nav-is--open');
+    $(appWrap).toggleClass('nav-is--open');
+    event.preventDefault();
+  }); 
+
+}
+
+// set tab index
+function tabIndexing(){
+
+  if ( $('html').attr('class') == 'nav-is--closed' ) {
+    $(sidebar).prop('tabIndex', 0);
+  } else {
+    $(sidebar).prop('tabIndex', -1);
+  }
+
+}
+
+$(sidebar).css({
+  'height': winHeight,
+});
+
+$(document).ready(function(){
+
+  openNavigation();
+  tabIndexing();
+
+});
+
+$(window).on('resize', function(){
+
+  $(sidebar).css({
+    'height': winHeight,
+  });
+
+});
