f6ec216e574789ed45b0e91d528a47659834b038
uxeskimo <uxeskimo@gmail.com>
2017-03-13 15:28:41 +0000

    Added js for feedabck feature

--- src/_js/main.js
+++ src/_js/main.js
@@ -1,5 +1,11 @@
+//= include includes/coop-toolkit.js
+//= include includes/feedback.js
+//= include includes/FeedbackWrongInfo.js
+
 $(function(){
 
+  Coop.init();
+  
   function showElement(element) {
     $(element).show().removeAttr('hidden').attr('aria-hidden', false);
     $(element).find(':input').prop('disabled', false);
@@ -72,34 +78,3 @@ $(function(){
     toggleInputLabelClass(this);
   });
 });
-
-(function() {
-
-  'use strict';
-
-  window.Coop = window.Coop || { Modules: {} };
-
-  Coop.Modules.feedbackForm = {
-      toggles:null,
-      toggleContent:null,
-      formPosition:null,
-      init:function(){
-          if($('#FeedbackForm .error').length)   // don't run if form-error
-              return;
-
-          this.toggles = $('#FeedbackForm input[type="radio"]');
-          this.toggleContent = $('#FeedbackForm .slide-toggle-content');
-          this.toggleContent.addClass('init').removeClass('open').attr({tabindex:'-1','aria-hidden':'true','aria-live':'polite'});
-          this.bindEvents();
-      },
-      bindEvents:function(){
-          var self = this;
-
-          this.toggles.change(function(){
-              self.formPosition = $('#FeedbackForm').offset().top;
-              self.toggleContent.slideDown(300).addClass('open').attr('aria-hidden','false');
-              $('html, body').animate({scrollTop:self.formPosition}, 300);
-          });
-      }
-  };
-}());
