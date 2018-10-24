e272a8df59d22ceebc16300a7a20f916bcd64d3b
Matt Tyas <matthewtyas@hotmail.com>
2018-10-24 15:58:58 +0100

    bundler update

--- gulpfile.js
+++ gulpfile.js
@@ -96,9 +96,9 @@ gulp.task('html', ['jekyll'], function() {
 });
 gulp.task('jekyll', function (gulpCallBack){
   var spawn = require('child_process').spawn;
-  var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit', cwd: 'src'});
   // Get Contenful content
   var contentful = spawn('jekyll', ['contentful']);
+  var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit', cwd: 'src'});
   jekyll.on('exit', function(code) {
     gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
   });
